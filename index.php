a85e92646c7017441a22a5eabc4c0dd8a26b9e05
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-19 13:24:13 +0200

    stampa degli hotel

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    
+
+<div>
+    <?php
+    
+    foreach($hotels as $elem){
+        echo "<div>" . $elem['name'] . "</div>";
+        echo "<div>" . $elem['description'] . "</div>";
+        echo "<div>" . "vote: " .  $elem['vote'] . "</div>";
+        echo "<div>" . "parcheggio" .  $elem['parking'] . "</div>";
+        echo "<div>" . "distance to center" .  $elem['distance_to_center'] . "</div>";
+
+    }
+    
+    ?>
+</div>
+
+
+</body>
+</html>
